b627087715237a9e717c5d2b9f56f932cf318486
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-04-16 18:39:41 -0500

    C++

--- main.cpp
+++ main.cpp
@@ -1,9 +1,24 @@
 #include <iostream>
+#include <string>
+#include <vector>
+
 
 int main() {
     std::cout << "¿Cómo te llamas?" << std::endl;
     // Este es un comentario
-    char x;
-    std::cin >> x;
+    std::string name;
+    std::cin >> name;
+
+    if (name == "Carlos" or name == "Miguel" or name == "Adelaida")
+    {
+        std::cout << "Somos del grupo de Deal.II.\n";
+    }
+    else{
+        std::cout << "No somos del grupo de Deal.II\n";
+    }
+
+    std::vector<int, 4> u {1, 2, 3, 4};
+    for (int i = 0; i < )
+
     return 0;
 }
