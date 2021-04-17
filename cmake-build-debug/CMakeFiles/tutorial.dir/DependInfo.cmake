84395b31bca329f76e752edc8a19a05371274ba6
Carlos Aznarán Laos <caznaranl@uni.pe>
2021-04-17 18:13:50 -0500

    Add example

--- cmake-build-debug/CMakeFiles/tutorial.dir/DependInfo.cmake
+++ cmake-build-debug/CMakeFiles/tutorial.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_CXX
-  "/tmp/tutorial/main.cpp" "/tmp/tutorial/cmake-build-debug/CMakeFiles/tutorial.dir/main.cpp.o"
+  "/tmp/c--example/main.cpp" "/tmp/c--example/cmake-build-debug/CMakeFiles/tutorial.dir/main.cpp.o"
   )
 set(CMAKE_CXX_COMPILER_ID "GNU")
 
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /tmp/tutorial
+# Install script for directory: /tmp/c--example
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/tmp/tutorial/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/tmp/c--example/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
